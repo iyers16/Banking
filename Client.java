9813f692d4310f0129af2d67cf10929d4ffd38cb
iyers16 <62408826+iyers16@users.noreply.github.com>
2021-09-20 14:09:54 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -6,39 +6,39 @@ public class Client implements IClient {
     private String firstName;
     private String lastName;
     private static int counter = 0;
-	private ArrayList<Account> accountList;
+		private ArrayList<Account> accountList;
 
-	public Client() {
-		Client.incrementClientCount;
-		this.id = this.counter;
-		this.firstName = NULL;
-		this.lastName = NULL;
-		this.accountList = new ArrayList<Account>();
-	}
+		public Client() {
+			Client.incrementClientCount;
+			this.id = this.counter;
+			this.firstName = NULL;
+			this.lastName = NULL;
+			this.accountList = new ArrayList<Account>();
+		}
 
-	public void addAccount(Account newAccount) {
-		this.accountList.add(newAccount);
-	}
+		public void addAccount(Account newAccount) {
+			this.accountList.add(newAccount);
+		}
 
-	public void displayAccounts() {
-		System.out.println("List of accounts:\n" + this.accountList);
-	}
+		public void displayAccounts() {
+			System.out.println("List of accounts:\n" + this.accountList);
+		}
 
-	public Account getAccount(int accountNumber) {
-		for(int i = 0; i < accountList.size(); i++) {
-			if(accountList.get(i).getAccountNumber() == accountNumber) {
-				return accountList.get(i);
+		public Account getAccount(int accountNumber) {
+			for(int i = 0; i < accountList.size(); i++) {
+				if(accountList.get(i).getAccountNumber() == accountNumber) {
+					return accountList.get(i);
+				}
 			}
 		}
-	}
 
-	@Override
-	String toString() {
-		String output = "\nClient ID: " + this.id +
-						"\nClient first name: " + this.firstName +
-						"\nClient last name: " + this.lastName;
-		return output;
-	}
+		@Override
+		String toString() {
+			String output = "\nClient ID: " + this.id +
+							"\nClient first name: " + this.firstName +
+							"\nClient last name: " + this.lastName;
+			return output;
+		}
 
     public final void setID(int id) {
         this.id = id;

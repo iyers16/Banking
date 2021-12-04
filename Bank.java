1df2825095c11a77f1515f8c9d3af44700476d7b
iyers16 <62408826+iyers16@users.noreply.github.com>
2021-12-04 18:44:29 -0500

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,54 +1,41 @@
-package banking;
+//done
+
+import java.util.ArrayList;
 
 public class Bank implements IBank {
 
-	private String bankNumber, address;
-	private ArrayList<Client> clientList;
+    private String bankNumber;
+    private String address;
+    private ArrayList<Client> clientList = new ArrayList<>();
+    
+    public Bank(String bnum, String adds) {
+        this.bankNumber = bnum;
+        this.address = adds;
+    }
 
-	public Bank() {
-		this.bankNumber = null;
-		this.address = null;
-		this.clientList = new ArrayList<Client>();
-	}
+    public void displayClientList() {
+        for (Client client : clientList) {
+            System.out.println(client);
+        }
+    }
 
-	public void addClient(Client newClient) {
-		this.clientList.add(newClient);
-	}
+    public Client getClient(int id) {
+        Client cli = this.clientList.stream()
+                                    .filter(client -> client.getId() == (id))
+                                    .findFirst()
+                                    .orElse(null);
+        return cli;
+    }
 
-	public void displayClientAccounts(int clientId) {
-		getClient(clientId).displayAccounts();
-	}
-
-	public void displayClientList() {
-		System.out.println("List of clients:\n" + this.clientList);
-	}
-
-	public Client getClient(int id) {
-		// for(int i = 0; i < clientList.size(); i++) {
-		// 	if(clientList.get(i).getID() == id) {
-		// 		return clientList.get(i);
-		// 	}
-		// }
-		return null;
-	}
-
-	public Account getClientAccount(int clientId, int accountNumber) {
-		// return this.getClient(clientId).getAccount(accountNumber);
-		return null;
-	}
-
-	public void setBankNumber(String bankNumber) {
-		this.bankNumber = bankNumber;
-	}
-	public String getBankNumber() {
-		return this.bankNumber;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getAddress() {
-		return this.address;
-	}
-
+    public ArrayList<Client> getClients() {return this.clientList;}
+    public void addClient(Client source) {this.clientList.add(source);}
+    public void setClients(ArrayList<Client> source) {this.clientList = source;}
+    public void displayClientAccounts(int cid) {getClient(cid).displayAccounts();}
+    public Account getClientAccount(int cid, int accn) {return getClient(cid).getAccount(accn);}
+    
+    public String getBankNumber() {return this.bankNumber;}
+    public void setBankNumber(String source) {this.bankNumber = source;}
+    
+    public String getAddress() {return this.address;}
+    public void setAddress(String source) {this.address = source;}
 }

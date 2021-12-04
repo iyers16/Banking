9f2ee84bd7a7185c9490e9f23fb0298f509c6bd0
iyers16 <62408826+iyers16@users.noreply.github.com>
2021-12-04 18:45:17 -0500

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,5 +1,7 @@
-package banking;
+//done
 
 public class CheckingAccount extends Account {
-    
+    public CheckingAccount(Client owner) {super(owner, TypeOfAcc.CHECKING);}
+
+    public CheckingAccount() {super(TypeOfAcc.CHECKING);}
 }
